b5ee0affb1920ba822b963c5934b49bb70d5a471
coso <wutongci@hotmail.com>
2019-07-15 18:17:01 +0800

    add dao domain

--- pkg/api/dao/domain.go
+++ pkg/api/dao/domain.go
@@ -1,5 +1,11 @@
 package dao
 
+import (
+	"github.com/jinzhu/gorm"
+	"zeus/pkg/api/dto"
+	"zeus/pkg/api/model"
+)
+
 type Domain struct {
 }
 
@@ -7,3 +13,40 @@ type Domain struct {
 //	db := GetDb()
 //	db.Where("code",role.Domain.Code)
 //}
+//Get - get single domain infoD
+func (u Domain) Get(id int) model.Domain {
+	var domain model.Domain
+	db.Where("id = ?", id).First(&domain)
+	return domain
+}
+
+// List - Domains list
+func (u Domain) List(listDto dto.GeneralListDto) ([]model.Domain, int64) {
+	var domains []model.Domain
+	var total int64
+	db := GetDb()
+	for sk, sv := range listDto.TransformSearch(dto.UserListSearchMapping) {
+		db = db.Where(fmt.Sprintf("%s = ?", sk), sv)
+	}
+	db.Preload("Domain").Offset(listDto.Offset).Limit(listDto.Limit).Find(&domains)
+	db.Model(&model.Domain{}).Count(&total)
+	return domains, total
+}
+
+// Create - new Domain
+func (u Domain) Create(domain *model.Domain) *gorm.DB {
+	db := GetDb()
+	return db.Save(domain)
+}
+
+// Update - update Domain
+func (u Domain) Update(domain *model.Domain) *gorm.DB {
+	db := GetDb()
+	return db.Save(domain)
+}
+
+// Create - new Domain
+func (u Domain) Delete(domain *model.Domain) *gorm.DB {
+	db := GetDb()
+	return db.Delete(domain)
+}
