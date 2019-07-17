0b76ff8280e27ce67d1f36ba8d28a5d0be7b4468
Hyman <zhengcog@gmail.com>
2019-07-18 00:37:39 +0800

    swagger doc

--- pkg/api/controllers/user.go
+++ pkg/api/controllers/user.go
@@ -61,7 +61,7 @@ func (u *UserController) List(c *gin.Context) {
 // @Security ApiKeyAuth
 // @Produce  json
 // @Success 200 {string} json "{"code":200,"data":{"id":1}}"
-// @Router /v1/api/users?limit=20&offset=0 [get]
+// @Router /users?limit=20&offset=0 [get]
 func (u *UserController) Create(c *gin.Context) {
 	var userDto dto.UserCreateDto
 	if u.BindAndValidate(c, &userDto) {
