5bc6cc7270e5bd1517e76c8ba6a38417a3de303f
coso <wutongci@hotmail.com>
2019-10-22 15:28:02 +0800

    update readme

--- scripts/getlatest.sh
+++ scripts/getlatest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir naftis
-curl -L https://api.github.com/repos/bullteam/zeus/tarball | tar -zx -C zeus --strip-components=1
+curl -L https://api.github.com/repos/bullteam/zeus-admin/tarball | tar -zx -C zeus --strip-components=1
 
 cd naftis
-curl -s https://api.github.com/repos/bullteam/zeus/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | wget -qi - && tar zxvf manifest.tar.gz
+curl -s https://api.github.com/repos/bullteam/zeus-admin/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | wget -qi - && tar zxvf manifest.tar.gz
 
